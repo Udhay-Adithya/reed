eab7101293190de04654fe540640616a8cb6d686
Udhay-Adithya <udhayxd@gmail.com>
2025-12-26 14:02:44 +0530

    feat: simplify dynamic color scheme logic

--- app/src/main/java/com/udhay/reed/ui/theme/Theme.kt
+++ app/src/main/java/com/udhay/reed/ui/theme/Theme.kt
@@ -1,7 +1,5 @@
 package com.udhay.reed.ui.theme
 
-import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
@@ -40,7 +38,7 @@ fun ReedTheme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+        dynamicColor -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
