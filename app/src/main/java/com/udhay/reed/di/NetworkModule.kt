4192d45d5e1f59790d93eeb1ff4cfc4ea1bf3057
Udhay-Adithya <udhayxd@gmail.com>
2025-12-28 21:00:49 +0530

    feat: add network module

---
+++ app/src/main/java/com/udhay/reed/di/NetworkModule.kt
@@ -0,0 +1,41 @@
+package com.udhay.reed.di
+
+import com.udhay.reed.data.network.ApiService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.core.annotation.Module
+import org.koin.core.annotation.Single
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+@Module
+class NetworkModule {
+
+    @Single
+    fun provideLoggingInterceptor() : HttpLoggingInterceptor{
+        return HttpLoggingInterceptor().apply {
+           level = HttpLoggingInterceptor.Level.BODY
+        }
+    }
+
+    @Single
+    fun provideHttpClient(loggingInterceptor: HttpLoggingInterceptor) : OkHttpClient{
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
+
+    @Single
+    fun provideRetrofit(httpClient: OkHttpClient): Retrofit{
+        return Retrofit.Builder()
+            .baseUrl("http://10.0.2.2:8080/")
+            .client(httpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Single
+    fun provideApiService(retrofit: Retrofit) : ApiService{
+        return retrofit.create(ApiService::class.java)
+    }
+}
