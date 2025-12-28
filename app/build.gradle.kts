26be398545b2a6ec28b1391bba1cbbf9112ffaba
Udhay-Adithya <udhayxd@gmail.com>
2025-12-28 13:30:51 +0530

    feat: add ksp and koin dependency
    - Add Koin and KSP dependencies and plugins to `libs.versions.toml`
    - Configure KSP plugin and Koin implementation in `app/build.gradle.kts`
    - Migrate Kotlin `jvmTarget` configuration to use `compilerOptions` DSL

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,10 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -33,8 +36,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions {
-        jvmTarget = "11"
+    kotlin {
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_11
+        }
     }
     buildFeatures {
         compose = true
@@ -58,4 +63,11 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     implementation(libs.androidx.core.splashscreen)
+    implementation(libs.koin.android)
+
+    // Koin
+    implementation(libs.koin.android)
+    ksp(libs.koin.ksp)
+    implementation(libs.koin.annotations)
+    ksp(libs.koin.ksp.compiler)
 }
