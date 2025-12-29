a22b6a1b5688768433dfd0ff73b0f8d54470af0a
Udhay-Adithya <udhayxd@gmail.com>
2025-12-29 09:39:22 +0530

    feat: add reusable Loader composable
    - Create `Loader.kt` in `util` package
    - Implement a centered `LoadingIndicator` composable
    - Add preview for the Loader component

---
+++ app/src/main/java/com/udhay/reed/util/Loader.kt
@@ -0,0 +1,29 @@
+package com.udhay.reed.util
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.LoadingIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
+@Composable
+fun Loader() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        LoadingIndicator()
+    }
+}
+
+@Preview
+@Composable
+fun LoaderPreview(){
+    Loader()
+}
