edd0d6ff6192eaca6b6bbdf64469053242037459
Udhay-Adithya <udhayxd@gmail.com>
2025-12-30 17:04:59 +0530

    feat: integrate api in login screen
    - Replace full-screen error and loading states with non-blocking UI
    - Integrate `SnackbarHost` and `SnackbarController` to display error messages via `AuthUiEffect`
    - Update login button to trigger `AuthUiEvent.Login` and display a loader while processing
    - Switch side-effect collection from `viewModel.events` to `viewModel.effect`
    - Clean up layout modifiers and text styling within the `Scaffold`

--- app/src/main/java/com/udhay/reed/ui/features/auth/LoginScreen.kt
+++ app/src/main/java/com/udhay/reed/ui/features/auth/LoginScreen.kt
@@ -1,6 +1,5 @@
 package com.udhay.reed.ui.features.auth
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,12 +11,16 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -34,6 +37,7 @@ import com.udhay.reed.ui.components.ErrorScreen
 import com.udhay.reed.ui.components.FullAppLogo
 import com.udhay.reed.ui.components.LabeledOutlinedTextField
 import com.udhay.reed.util.Loader
+import com.udhay.reed.util.SnackbarController
 import kotlinx.coroutines.flow.collectLatest
 import org.koin.compose.viewmodel.koinViewModel
 
@@ -41,153 +45,156 @@ import org.koin.compose.viewmodel.koinViewModel
 @Composable
 fun LoginScreen(
     onLoginSuccess: () -> Unit,
-    onNavigateToSignup: ((link: LinkAnnotation) -> Unit),
+    onNavigateToSignup: (LinkAnnotation) -> Unit,
     viewModel: AuthViewModel = koinViewModel()
 ) {
+    val scope = rememberCoroutineScope()
     LaunchedEffect(Unit) {
-        viewModel.events.collectLatest { event ->
+        viewModel.effect.collectLatest { event ->
             when (event) {
-                // TODO handle events
-                is AuthUiEvent.ShowErrorMessage -> TODO()
-            }
-        }
-    }
 
-    val state = viewModel.state.collectAsStateWithLifecycle()
-    var email: String by remember { mutableStateOf("") }
-    var password: String by remember { mutableStateOf("") }
+                AuthUiEffect.NavigateToHome -> {
+                    onLoginSuccess()
+                }
 
-
-    when (val uiState = state.value) {
-        is AuthUiState.Loading -> {
-            Loader()
-        }
-
-        is AuthUiState.Error -> {
-            val errorMessage = (state.value as AuthUiState.Error).message
-            ErrorScreen(
-                errorMessage,
-                "Retry",
-                onPrimaryButtonClicked = {}
-            ) { }
-        }
-
-        is AuthUiState.Success -> {
-
-            Scaffold() { paddingValues ->
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(paddingValues)
-                        .padding(16.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Top
-
-                ) {
-                    Spacer(
-                        modifier = Modifier.height(12.dp)
+                is AuthUiEffect.ShowErrorMessage -> {
+                    SnackbarController.show(
+                        scope = scope,
+                        message = event.message,
+                        duration = SnackbarDuration.Short
                     )
-                    FullAppLogo()
-                    Spacer(
-                        modifier = Modifier.height(12.dp)
-                    )
-                    Text(
-                        text = "Great to see\nyou!",
-                        textAlign = TextAlign.Center,
-                        style = MaterialTheme.typography.headlineLarge,
-                        fontWeight = FontWeight.Bold
-                    )
-                    Spacer(
-                        modifier = Modifier.height(12.dp)
-                    )
-
-                    Text(
-                        text = "Log in and dive into your\nuniverse of tunes",
-                        style = MaterialTheme.typography.bodyLarge,
-                        textAlign = TextAlign.Center,
-                        color = MaterialTheme.colorScheme.outline
-
-                    )
-
-                    Spacer(
-                        modifier = Modifier.height(48.dp)
-                    )
-
-                    LabeledOutlinedTextField(
-                        label = "Email",
-                        value = email,
-                        onValueChange = { email = it },
-                        placeholder = "Email",
-                        keyboardType = KeyboardType.Email
-                    )
-
-
-                    Spacer(
-                        modifier = Modifier.height(28.dp)
-                    )
-
-                    LabeledOutlinedTextField(
-                        label = "Password",
-                        value = password,
-                        onValueChange = { password = it },
-                        placeholder = "Password",
-                        isPassword = true,
-                        keyboardType = KeyboardType.Password
-                    )
-
-                    Spacer(
-                        modifier = Modifier.height(36.dp)
-                    )
-
-                    Button(
-                        onClick = onLoginSuccess,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(56.dp),
-                        shape = ButtonDefaults.squareShape,
-                        colors = ButtonDefaults.buttonColors(
-                            containerColor = MaterialTheme.colorScheme.primary,
-                        ),
-
-
-                        ) {
-                        Text(
-                            "Login",
-                            style = MaterialTheme.typography.bodyLarge,
-                            textAlign = TextAlign.Start,
-                            fontWeight = FontWeight.Bold
-                        )
-                    }
-
-                    Spacer(Modifier.weight(1f))
-
-                    val annotatedText = buildAnnotatedString {
-                        append("Don't have an account? ")
-
-                        pushLink(
-                            LinkAnnotation.Clickable(
-                                tag = "register",
-                                linkInteractionListener = onNavigateToSignup
-                            )
-                        )
-                        withStyle(
-                            SpanStyle(
-                                color = MaterialTheme.colorScheme.primary,
-                                fontWeight = FontWeight.SemiBold
-                            )
-                        ) {
-                            append("Register")
-                        }
-                        pop()
-                    }
-
-                    Text(
-                        text = annotatedText,
-                        style = MaterialTheme.typography.bodyMedium
-                    )
-
                 }
             }
         }
     }
+
+    val snackbarHostState = remember { SnackbarHostState() }
+
+    SnackbarController.init(snackbarHostState)
+
+    val state by viewModel.state.collectAsStateWithLifecycle()
+
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+//    if (state is AuthUiState.Error) {
+//        ErrorScreen(
+//            errorMessage = (state as AuthUiState.Error).message,
+//            primaryButton = "Retry",
+//            onPrimaryButtonClicked = {
+//                // viewModel.retry()
+//            }
+//        ) {}
+//        return
+//    }
+
+    val isLoading = state is AuthUiState.Loading
+
+    Scaffold(
+        snackbarHost = { SnackbarHost(snackbarHostState) }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Spacer(Modifier.height(12.dp))
+            FullAppLogo()
+
+            Spacer(Modifier.height(12.dp))
+            Text(
+                text = "Great to see\nyou!",
+                style = MaterialTheme.typography.headlineLarge,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            )
+
+            Spacer(Modifier.height(12.dp))
+            Text(
+                text = "Log in and dive into your\nuniverse of tunes",
+                style = MaterialTheme.typography.bodyLarge,
+                textAlign = TextAlign.Center,
+                color = MaterialTheme.colorScheme.outline
+            )
+
+            Spacer(Modifier.height(48.dp))
+
+            LabeledOutlinedTextField(
+                label = "Email",
+                value = email,
+                onValueChange = { email = it },
+                placeholder = "Email",
+                keyboardType = KeyboardType.Email,
+            )
+
+            Spacer(Modifier.height(28.dp))
+
+            LabeledOutlinedTextField(
+                label = "Password",
+                value = password,
+                onValueChange = { password = it },
+                placeholder = "Password",
+                isPassword = true,
+                keyboardType = KeyboardType.Password
+            )
+
+            Spacer(Modifier.height(36.dp))
+
+            Button(
+                onClick = {
+                    viewModel.onEvent(
+                        AuthUiEvent.Login(
+                            email = email,
+                            password = password
+                        )
+                    )
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(56.dp),
+                enabled = !isLoading,
+                shape = ButtonDefaults.squareShape
+            ) {
+                if (isLoading) {
+                    Loader()
+                } else {
+                    Text(
+                        "Login",
+                        style = MaterialTheme.typography.bodyLarge,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+            }
+
+            Spacer(Modifier.weight(1f))
+
+            val annotatedText = buildAnnotatedString {
+                append("Don't have an account? ")
+                pushLink(
+                    LinkAnnotation.Clickable(
+                        tag = "register",
+                        linkInteractionListener = onNavigateToSignup
+                    )
+                )
+                withStyle(
+                    SpanStyle(
+                        color = MaterialTheme.colorScheme.primary,
+                        fontWeight = FontWeight.SemiBold
+                    )
+                ) {
+                    append("Register")
+                }
+                pop()
+            }
+
+            Text(
+                text = annotatedText,
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+    }
 }
+
