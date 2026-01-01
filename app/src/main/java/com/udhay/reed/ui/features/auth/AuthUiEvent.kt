797f3a4de2022a017ffe32d0998c0d3547b3ddf2
Udhay-Adithya <udhayxd@gmail.com>
2026-01-01 22:26:57 +0530

    feat: add Signup event to AuthUiEvent for user registration

--- app/src/main/java/com/udhay/reed/ui/features/auth/AuthUiEvent.kt
+++ app/src/main/java/com/udhay/reed/ui/features/auth/AuthUiEvent.kt
@@ -1,5 +1,6 @@
 package com.udhay.reed.ui.features.auth
 sealed interface AuthUiEvent {
     data class Login(val email: String, val password: String) : AuthUiEvent
+    data class Signup(val name : String, val email: String, val password: String) : AuthUiEvent
 
 }
