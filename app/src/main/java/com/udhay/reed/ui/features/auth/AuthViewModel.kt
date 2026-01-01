c9f4e2954f574e2051eb0d1763adcec0b03a7abf
Udhay-Adithya <udhayxd@gmail.com>
2026-01-01 22:27:04 +0530

    feat: add signup handling in AuthViewModel with validation and state management

--- app/src/main/java/com/udhay/reed/ui/features/auth/AuthViewModel.kt
+++ app/src/main/java/com/udhay/reed/ui/features/auth/AuthViewModel.kt
@@ -3,6 +3,7 @@ package com.udhay.reed.ui.features.auth
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.udhay.reed.data.model.LoginRequest
+import com.udhay.reed.data.model.RegisterRequest
 import com.udhay.reed.data.network.Resource
 import com.udhay.reed.data.repository.AuthRepository
 import kotlinx.coroutines.flow.MutableSharedFlow
@@ -25,6 +26,7 @@ class AuthViewModel(val repository: AuthRepository) : ViewModel() {
     fun onEvent(event: AuthUiEvent){
         when(event){
             is AuthUiEvent.Login -> login(event.email, event.password)
+            is AuthUiEvent.Signup -> signup(event.name, event.email, event.password)
         }
     }
 
@@ -41,6 +43,36 @@ class AuthViewModel(val repository: AuthRepository) : ViewModel() {
             }
 
             val res = repository.loginUser(LoginRequest(email, password))
+
+            when(res){
+                is Resource.Error -> {
+                    _state.value = AuthUiState.Error(res.message)
+                    _effect.emit(
+                        AuthUiEffect.ShowErrorMessage(res.message))
+                }
+                is Resource.Success -> {
+                    _state.value = AuthUiState.Success
+                    _effect.emit(AuthUiEffect.NavigateToHome)
+
+                }
+            }
+
+        }
+    }
+
+    private fun signup(name: String, email: String, password: String){
+        viewModelScope.launch {
+            _state.value = AuthUiState.Loading
+
+            if (email.isBlank() || password.isBlank() || name.isBlank()) {
+                _state.value = AuthUiState.Error("Invalid credentials")
+                _effect.emit(
+                    AuthUiEffect.ShowErrorMessage("Email or password cannot be empty")
+                )
+                return@launch
+            }
+
+            val res = repository.registerUser(RegisterRequest(name, email, password))
 
             when(res){
                 is Resource.Error -> {
