c7f608c82cbd537c82ba6c8129086d5b5fca4e00
Udhay-Adithya <udhayxd@gmail.com>
2026-01-20 12:53:11 +0530

    feat: add data model classes for Album, Artist, Song, and HomeDataResponse

---
+++ app/src/main/java/com/udhay/reed/data/model/Artist.kt
@@ -0,0 +1,10 @@
+package com.udhay.reed.data.model
+
+data class Artist(
+    val bio: String,
+    val createdAt: Long,
+    val id: String,
+    val name: String,
+    val profilePicture: String,
+    val updatedAt: Long
+)
